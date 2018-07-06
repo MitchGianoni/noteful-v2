2743224af28c29eca26c8739229d7c5e66574818
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-06 01:52:51 -0400

    knex exercise complete

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,6 +2,7 @@
 
 const knex = require('../knex');
 
+// Get notes including searchTerm
 let searchTerm = 'gaga';
 knex
   .select('notes.id', 'title', 'content')
@@ -17,4 +18,59 @@ knex
   })
   .catch(err => {
     console.error(err);
+  });
+
+// Get a note by ID
+let getId = 1008;
+knex()
+  .select('notes.id', 'title', 'content')
+  .from('notes')
+  .where({id: getId})
+  .then(results =>{
+    console.log(results[0]);
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+
+// Update a note by id
+let getId2 = 1005;
+let updateObj = {title: 'Who cares about Mike Green', content: 'He is no longer relevant and the wings are awful.'};
+knex()
+  .from('notes')
+  .where({id: getId2})
+  .update(updateObj)
+  .returning(['id', 'title', 'content'])
+  .then(results =>{
+    console.log(results[0]);
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+
+// Create a new note
+let newObj = {title: 'Tavares to the Maple Leafs', content: 'The Northeast may have a new contender!'};
+knex()
+  .from('notes')
+  .insert(newObj)
+  .returning(['id','title','content'])
+  .then(results =>{
+    console.log(results[0]);
+  })
+  .catch(err =>{
+    console.log(err);
+  });
+
+
+// Delete a note by ID
+let id = 1005;
+knex()
+  .from('notes')
+  .where('id', id)
+  .del()
+  .then(results =>{
+    console.log('Deleted: ', results);
+  })
+  .catch(err =>{
+    console.log(err);
   });
