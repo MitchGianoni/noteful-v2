5758302b8c8fe2444ce4009684c0085441d2c707
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-09 00:30:11 -0400

    added tags router to server.js and added tags.js, get all tags and post a new tag functional

---
+++ routes/tags.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// Use EXPRESS
+const express = require('express');
+// Create an router instance (aka "mini-app")
+const router = express.Router();
+// Use knex
+const knex = require('../knex');
+
+// Get all tags
+router.get('/', (req, res, next) => {
+  knex.select('id', 'name')
+    .from('tags')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => next(err));
+});
+
+// // Get tag by id
+// router.get('/:id', (req, res, next) => {
+// });
+
+/* ========== POST/CREATE ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  /***** Never trust users. Validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = { name };
+
+  knex.insert(newItem)
+    .into('tags')
+    .returning(['id', 'name'])
+    .then((results) => {
+      // Uses Array index solution to get first item in results array
+      const result = results[0];
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
